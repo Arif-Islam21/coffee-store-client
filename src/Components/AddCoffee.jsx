e1dd01748e1a0effa1017fb510b3864b1291c749
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-16 02:41:22 +0530

    designing the project

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -1,5 +1,39 @@
 const AddCoffee = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Add COffee</h2>
+      <form>
+        <div className="flex">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <label className="input">
+              <span>Name</span>
+              <input
+                type="text"
+                placeholder="Coffee Name"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <label className="input">
+              <span>Name</span>
+              <input
+                type="text"
+                placeholder="Available Coffee"
+                className="input input-bordered"
+              />
+            </label>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
