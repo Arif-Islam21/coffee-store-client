d5dad2c74298ddcd72b0dc296b53a316eff2e365
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-05-17 02:16:56 +0530

    delete operation done

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -1,5 +1,38 @@
+import Swal from "sweetalert2";
+
 const CoffeeCard = ({ coffee }) => {
-  const { name, quantity, supplier, taste, category, details, photo } = coffee;
+  const { _id, name, quantity, supplier, taste, category, details, photo } =
+    coffee;
+
+  const handleDelete = (_id) => {
+    console.log(_id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/coffee/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          });
+      }
+    });
+  };
 
   return (
     <div className="card card-side bg-base-100 shadow-xl">
@@ -18,12 +51,13 @@ const CoffeeCard = ({ coffee }) => {
             <button className="grid w-24 h-12 my-2 rounded bg-primary text-primary-content place-content-center">
               View
             </button>
-
             <button className="grid w-24 h-12 my-2 rounded bg-accent text-accent-content place-content-center">
               Edit
             </button>
-
-            <button className="grid w-24 h-12 my-2 rounded bg-secondary text-secondary-content place-content-center">
+            <button
+              onClick={() => handleDelete(_id)}
+              className="grid w-24 h-12 my-2 rounded bg-secondary text-secondary-content place-content-center"
+            >
               Delete
             </button>
           </div>
